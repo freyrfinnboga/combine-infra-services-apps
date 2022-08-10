8097719262ae2a2547eae440e3fb787a42ae33bb
ori.freyrf <freyr.finnbogason@origo.is>
2022-08-10 15:09:35 +0000

    add sh

--- combine-infra-services-apps.sh
+++ combine-infra-services-apps.sh
@@ -23,7 +23,7 @@
 # Fork it!
 
 # Replace `[...]` with the GitHub organization or the username
-$GH_ORG='freyrfinnboga'
+export GH_ORG=[...]
 
 git clone https://github.com/$GH_ORG/combine-infra-services-apps.git
 
@@ -33,21 +33,17 @@ cd combine-infra-services-apps
 # Setup: Controller Cluster #
 #############################
 
-<!-- export KUBECONFIG=$PWD/kubeconfig.yaml -->
+export KUBECONFIG=$PWD/kubeconfig.yaml
 
 # Feel free to use any other Kubernetes cluster
-<!-- kind create cluster --config kind.yaml -->
+kind create cluster --config kind.yaml
 
 # NGINX Ingress installation might differ for your k8s provider
-kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-
-kubectl get services -n ingress-nginx
-# NAME                                 TYPE        CLUSTER-IP      EXTERNAL-IP   PORT(S)                      AGE
-# ingress-nginx-controller             NodePort    10.43.248.227   <none>        80:31224/TCP,443:31432/TCP   107s
-# ingress-nginx-controller-admission   ClusterIP   10.43.192.196   <none>        443/TCP  
+kubectl apply \
+    --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 # If not using kind, replace `127.0.0.1.nip.io` with the base host accessible through NGINX Ingress
-$BASE_HOST='10.43.192.196.nip.io'
+export BASE_HOST=127.0.0.1.nip.io
 
 ############################
 # Setup: Deploy Crossplane #
@@ -55,56 +51,64 @@ $BASE_HOST='10.43.192.196.nip.io'
 
 # Watch https://youtu.be/n8KjVmuHm7A if you are not familiar with Crossplane
 
-helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo add crossplane-stable \
+    https://charts.crossplane.io/stable
 
 helm repo update
 
-helm upgrade --install crossplane crossplane-stable/crossplane --namespace crossplane-system --create-namespace --wait
+helm upgrade --install \
+    crossplane crossplane-stable/crossplane \
+    --namespace crossplane-system \
+    --create-namespace \
+    --wait
 
 ##############
 # Setup: GCP #
 ##############
 
-$PROJECT_ID="devops-toolkit-$(Get-Date -Format "yyyyMMdd")"
-
-Install-Module GoogleCloud -Scope CurrentUser
-Import-Moudle GoogleCloud
+export PROJECT_ID=devops-toolkit-$(date +%Y%m%d%H%M%S)
 
 gcloud projects create $PROJECT_ID
 
-Write-Output "https://console.cloud.google.com/marketplace/product/google/container.googleapis.com?project=$PROJECT_ID"
+echo https://console.cloud.google.com/marketplace/product/google/container.googleapis.com?project=$PROJECT_ID
 
 # Open the URL and *ENABLE* the API
 
-Write-Output "https://console.developers.google.com/apis/library/sqladmin.googleapis.com?project=$PROJECT_ID"
+echo https://console.developers.google.com/apis/library/sqladmin.googleapis.com?project=$PROJECT_ID
 
 # Open the URL and *ENABLE* the API
 
-$SA_NAME='devops-toolkit'
+export SA_NAME=devops-toolkit
 
-$SA="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+export SA="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 
-gcloud iam service-accounts create $SA_NAME --project $PROJECT_ID
+gcloud iam service-accounts \
+    create $SA_NAME \
+    --project $PROJECT_ID
 
-$ROLE='roles/admin'
+export ROLE=roles/admin
 
-gcloud projects add-iam-policy-binding --role $ROLE $PROJECT_ID --member serviceAccount:$SA
+gcloud projects add-iam-policy-binding \
+    --role $ROLE $PROJECT_ID \
+    --member serviceAccount:$SA
 
-gcloud iam service-accounts keys create creds.json --project $PROJECT_ID --iam-account $SA
+gcloud iam service-accounts keys \
+    create creds.json \
+    --project $PROJECT_ID \
+    --iam-account $SA
 
-kubectl --namespace crossplane-system create secret generic gcp-creds --from-file key=./creds.json
+kubectl --namespace crossplane-system \
+    create secret generic gcp-creds \
+    --from-file key=./creds.json
 
-#plugin-ið var ekki komið og til að installa því þá er það þessi skipun
-curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh | sh
-#sem virkar ekki í powershell en ég fann ekki hvernig hægt er að install plugin-inu auðveldlega með windows/ps
-
-kubectl crossplane install provider crossplane/provider-gcp:v0.15.0
+kubectl crossplane install provider \
+    crossplane/provider-gcp:v0.15.0
 
 kubectl get providers
 
 # Repeat the previous command until `HEALTHY` column is set to `True`
 
-Write-Output "apiVersion: gcp.crossplane.io/v1beta1
+echo "apiVersion: gcp.crossplane.io/v1beta1
 kind: ProviderConfig
 metadata:
   name: default
@@ -115,7 +119,8 @@ spec:
     secretRef:
       namespace: crossplane-system
       name: gcp-creds
-      key: key" | kubectl apply --filename -
+      key: key" \
+    | kubectl apply --filename -
 
 #########################
 # Setup: Deploy Argo CD #
@@ -123,18 +128,37 @@ spec:
 
 # Watch https://youtu.be/vpWQeoaiRM4 if you are not familiar with Argo CD
 
-cat argo-cd/base/ingress.yaml | sed -e "s@acme.com@argo-cd.$BASE_HOST@g" | tee argo-cd/overlays/production/ingress.yaml
-cat controller/argo-cd.yaml | sed -e "s@vfarcic@$GH_ORG@g" | tee controller/argo-cd.yaml
-cat controller/gke.yaml | sed -e "s@vfarcic@$GH_ORG@g" | tee controller/gke.yaml
-cat controller/devops-toolkit.yaml | sed -e "s@vfarcic@$GH_ORG@g" | tee controller/devops-toolkit.yaml
-cat apps.yaml | sed -e "s@vfarcic@$GH_ORG@g" | tee apps.yaml
+cat argo-cd/base/ingress.yaml \
+    | sed -e "s@acme.com@argo-cd.$BASE_HOST@g" \
+    | tee argo-cd/overlays/production/ingress.yaml
+
+cat controller/argo-cd.yaml \
+    | sed -e "s@vfarcic@$GH_ORG@g" \
+    | tee controller/argo-cd.yaml
+
+cat controller/gke.yaml \
+    | sed -e "s@vfarcic@$GH_ORG@g" \
+    | tee controller/gke.yaml
+
+cat controller/devops-toolkit.yaml \
+    | sed -e "s@vfarcic@$GH_ORG@g" \
+    | tee controller/devops-toolkit.yaml
+
+cat apps.yaml \
+    | sed -e "s@vfarcic@$GH_ORG@g" \
+    | tee apps.yaml
+
 git add .
+
 git commit -m "Initial commit"
+
 git push
 
 # Watch https://youtu.be/Twtbg6LFnAg if you are not familiar with Kustomize
 
-kustomize build argo-cd/overlays/production | kubectl apply --filename -
+kustomize build \
+    argo-cd/overlays/production \
+    | kubectl apply --filename -
 
 kubectl --namespace argocd \
     rollout status \
